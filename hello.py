03a37ad01aa099b9cff70cbd3e40fce486f1de1f
paulinohuerta <paulinohuerta@gmail.com>
2017-04-25 22:30:14 +0200

    version 1b

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -6,10 +6,16 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/person/<a_name>')
+def person(a_name):
+    return '<h1>Hello, %s</h1>' % a_name
+
+@app.route('/login')
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return '<h1>Hello, %s </h1><h3>password=%s</h3>' % (username, password)
 
 if __name__ == '__main__':
-    # I am interested in using the port 5003, if it does not indicate
-    # the 5000 takes by default.
-    # debug enabled means activate debugger and reloader.
     app.run(port=5003,debug=True)
 
