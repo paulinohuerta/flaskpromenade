002bb591f67660036e9fc5a38a2e65cf411a9d88
paulinohuerta <paulinohuerta@gmail.com>
2017-05-03 14:10:53 +0200

    version 1f

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
 @app.route('/person/<name>')
 def person(name):
     return render_template('person.html',a_name=name)
+
+@app.route('/advices')
+def advices():
+    data = [
+       'Always finish what you started',
+       'Do what you\'re doing your best',
+       'Do not cling to anything that will eventually destroy you'
+    ]
+    return render_template('advices.html', comments=data)
 
 if __name__ == '__main__':
     app.run(port=5003,debug=True)
