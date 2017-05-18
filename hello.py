2c3e35d6a930f82da85c0919d65efc01d8f83df4
paulinohuerta <paulinohuerta@gmail.com>
2017-05-18 06:37:47 +0200

    Using flask-bootstrap and flask-wtf

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
@@ -19,8 +24,8 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-      name = form.name.data
-      form.name.data = ''
+        name = form.name.data
+        form.name.data = ''
     return render_template('index.html', form=form, name=name)
 
 if __name__ == '__main__':
